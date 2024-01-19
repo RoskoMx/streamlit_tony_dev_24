e72ca8569f596290b671a8b8daddfd0ff88c1ef4
RoskoMx <78753834+RoskoMx@users.noreply.github.com>
2024-01-19 12:08:17 -0600

    Create url.py

---
+++ url.py
@@ -0,0 +1,18 @@
+#Librería que acorta la URL
+#Librería que crea la página web
+import pyshorteners
+import streamlit as st
+
+def shorten_url(url): #Función que recibe la URL
+    s = pyshorteners.Shortener() #Creamos un objeto de esta librería
+    shortened_url = s.tinyurl.short(url) #Llamamos al método que nos acorta la URL
+    return shortened_url
+
+#creamos app web con stremamlit
+#Configuración de la página: título, el ícono y la forma en cómo se verá la página
+st.set_page_config(page_title="URL Shortener", page_icon="✏️", layout="centered")
+st.image("imagenes\rosko.jpg", use_column_width=True)
+st.title("URL Shortener")
+url = st.text_input("Enter the URL")
+if st.button("Generate new URL"):
+    st.write("URL shortened: ", shorten_url(url))
